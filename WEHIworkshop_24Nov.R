375d8cda9edb6077513cca72dd3994e5f1cb5e3f
alistairwalsh <alistair.walsh@me.com>
2015-11-10 16:00:40 +1100

    Initial commit

---
+++ WEHIworkshop_24Nov.R
@@ -0,0 +1,187 @@
+# PART 1: R Demo ------------------------------------------------------
+
+# Example Dataset saved as "Cytokines_24Nov.csv"
+# Import Data
+read.csv(file="/Users/keeble.j/Desktop/Cytokines_24Nov.csv", header = TRUE)
+
+# store dataset in R
+rawCytokines <- read.csv(file="/Users/keeble.j/Desktop/Cytokines_24Nov.csv", header = TRUE)
+
+# transform dataset e.g. log2
+log2Cytokines <- log2(rawCytokines[,c(3,4)])
+log2Cytokines
+
+# generate final data.frame
+Cytokines <- data.frame(rawCytokines[,c(1:2)], log2Cytokines)
+Cytokines
+
+# Compare the groups
+
+# Identify the groups (Control,Treated)
+groups <- Cytokines$Group
+groups[grep("Control", groups)] <- "Control"
+groups[grep("Treated", groups)] <- "Treated"
+
+# Perform T Test
+IL17F_TTest <- t.test(Cytokines$IL17F ~groups)
+IL17A_TTest <- t.test(Cytokines$IL17A ~groups)
+
+# Perform Multiple T Tests at once
+Cytokines_TTests <- lapply(1:dim(log2Cytokines)[2], function(i) {t.test(log2Cytokines[,i] ~groups)})
+Cytokines_TTests
+names(Cytokines_TTests) <- colnames(Cytokines[,c(3:4)])
+Cytokines_TTests
+
+# Plot Data
+
+
+
+
+
+
+# PART 2: Hands-On Session ------------------------------------------------
+
+
+# Possible Coding which might be useful for the Hands-On Part of Workshop?
+
+
+# Functions ---------------------------------------------------------------
+# call a funtion by using its name(), e.g.
+sum(2, 3, 5)
+# store results by assigning a function to a variable, e.g.
+sum.of.three.numbers <- sum(2,3,5)
+
+# Getting Help ------------------------------------------------------------
+help(sum)
+example(sum)
+
+# Vectors -----------------------------------------------------------------
+# vector values must be the same type e.g. all numbers
+# variable x with numbers between 2 and 4 (again using similar format to excel)
+x <- 2:4
+# variable y with numbers 3, 6, 8 (using 'c' to combine)
+y <- c(3,6,8)
+# perform operation on a vector, e.g.
+y
+2 * y
+# add two vectors together
+x + y
+# compare vectors
+x < y
+x == c(2, 5, 10)
+
+# extract individual values from vectors using [position], e.g.
+celltypes <- c('B cells', 'T cells', 'Neutrophils', 'Monocytes')
+celltypes[3]
+
+# new values can be assigned to an existing vector, e.g.
+celltypes[2] <- "CD4 T cells"
+
+# add more values to the end of a vector
+celltypes[5] <- "CD8 T cells"
+
+# retrieve values from vector
+celltypes[3:4]
+
+# assign names to vector values, e.g
+y
+names(y) <- c("DCs", "monocytes", "macrophages")
+y
+# retrieve values using names()
+y["monocytes"]
+
+
+# Barplots ---------------------------------------------------------
+infiltrating.cells <- c(30, 50, 120)
+names(infiltrating.cells) <- c('T cells', 'B cells', 'Neutrophils')
+barplot(infiltrating.cells)
+# can full cutomize graph output (e.g. adjust size of text, add titles, change colors of bars, etc) - see help(barplots)
+barplot(infiltrating.cells, main = "Infiltrating Cells", col = rainbow(20), cex.main = 1.8, cex.names = 1.5, cex.axis = 1.4, font = 1)
+
+
+# Scatter Plots -----------------------------------------------------------
+x <- c(1, 3, 4, 6, 7, 10, 15, 16, 20, 30, 42, 55, 56, 64)
+y <- sqrt(x)
+plot(x,y)
+help("plot")
+# again, you can fully customize your plot e.g. change from points to stairs
+plot (x,y, type = "s")
+# or change colors of points
+plot(x,y, type = "s", col = 'blue')
+help("plot")
+# see also par() for more info on graphical parameters
+par()
+
+
+# Matrices ----------------------------------------------------------------
+# Add the number 1:15 to matrix with 5 rows x 3 columns
+matrix(data=1:15, nrow = 5, ncol = 3)
+# also can be done,
+x <- 1:15
+matrix(x,5,3)
+# you can also re-shape a vector into a new matrix
+dim(x) <- c(3,5)
+x
+
+# Accessing values from Matrix
+x
+x[2,3]   # row 2, column 3
+
+# can assign new values within a matrix
+x[2,3] <- 20
+x
+
+#retrieve entire row
+x[2,]
+# or column
+x[,3]
+
+
+# Factors -----------------------------------------------------------------
+# Use the 'factor' function to group data into categories
+# e.g. to organise cell.type data collected from tumors
+tumors <- c('Neut', 'Macs', 'B cells', 'T cells')
+cell.types <- factor(tumors)
+
+# factors helps you to see the number of unique 'Levels'
+tumors
+cell.types
+levels(cell.types)
+
+
+# Dataframes --------------------------------------------------------------
+cell.types
+cell.numbers <- c(100, 200, 20, 30)
+identification.marker <- c('Ly6G','F4/80', 'B220', 'CD3')
+tumor.infiltrating.immune.cells <- data.frame(cell.types, cell.numbers, identification.marker)
+tumor.infiltrating.immune.cells
+
+# retrieve columns
+tumor.infiltrating.immune.cells[[2]]
+tumor.infiltrating.immune.cells[["cell.numbers"]]
+# quickest way is to use '$'
+tumor.infiltrating.immune.cells$cell.numbers
+
+
+# Import Data into R ------------------------------------------------------
+# can use 'Import Dataset' button or type commands:
+read.csv(file="/Users/keeble.j/Desktop/Cytokines_24Nov.csv", header = TRUE)
+
+# store dataset in R
+rawCytokines <- read.csv(file="/Users/keeble.j/Desktop/Cytokines_24Nov.csv", header = TRUE)
+
+# transform dataset e.g. log2
+log2Cytokines <- log2(rawCytokines[,c(3,4)])
+log2Cytokines
+
+# generate final data.frame
+Cytokines <- data.frame(rawCytokines[,c(1:2)], log2Cytokines)
+Cytokines
+
+# Compare the groups
+
+# Identify the groups (Control,Treated)
+groups <- Cytokines$Group
+groups[grep("Control", groups)] <- "Control"
+groups[grep("Treated", groups)] <- "Treated"
+
